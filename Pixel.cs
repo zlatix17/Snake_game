e5239ea2d11c0f310da3e5858b0a31f91fd5cacc
Zlatka Dimitrova <113666852+zlatix17@users.noreply.github.com>
2023-01-02 18:55:43 +0200

    Add project files.

---
+++ Pixel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Pixel
+    {
+        public Pixel(int xPos, int yPos, ConsoleColor color)
+        {
+            XPos = xPos;
+            YPos = yPos;
+            ScreenColor = color;
+        }
+        public int XPos { get; set; }
+        public int YPos { get; set; }
+        public ConsoleColor ScreenColor { get; set; }
+    }
+}
